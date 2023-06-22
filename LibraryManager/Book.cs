445d48cc058dd0d5d14470fc0719e55865622aff
zheni4ka <ebranik15@gmail.com>
2023-06-22 23:58:13 +0300

    Add project files.

---
+++ LibraryManager/Book.cs
@@ -0,0 +1,31 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Book
+    {
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public string Description { get; set; }
+        public int YearRelease { get; set; }
+
+        public Book(string name, string authorName, string description, int yearRelease)
+        {
+            Name = name;
+            AuthorName = authorName;
+            Description = description;
+            YearRelease = yearRelease;
+        }
+
+        [DependsOn("Name", "AuthorName", "YearRelease")]
+        public string Bio => $"\"{Name}\"  by {AuthorName}               {YearRelease}";
+
+        public Book() { }
+    }
+}
---
+++ LibraryManager/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LibraryManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel viewModel = new ViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+    }
+
+
+
+    [AddINotifyPropertyChangedInterface]
+    public class ViewModel
+    {
+        public bool checkPrev(Book currBook) { return books.ElementAt(books.IndexOf(currBook) - 1) == null; }
+        public bool checkNext(Book currBook) { return books.ElementAt(books.IndexOf(currBook) + 1) == null; }
+        
+        private ObservableCollection<Book> books = new ObservableCollection<Book>();
+        public IEnumerable<Book> Books => books;
+
+        private RelayCommand nextBook;
+        private RelayCommand previusBook;
+        public ICommand NextBook_ => nextBook;
+        public ICommand PreviousBook_ => previusBook;
+
+        void NextBook() { SelectedBook = books[books.IndexOf(SelectedBook) + 1]; }
+        void PrevBook() { SelectedBook = books[books.IndexOf(SelectedBook) - 1]; }
+
+        public Book SelectedBook { get; set; }
+        public ViewModel()
+        {
+            books.Add(new Book("Chorna Rada", "Panteleymon Kulish", "a historical novel by the Ukrainian writer Panteleimon Kulish, which depicts a well-known historical event - the black council that took place in Nizhyn in 1663, recreates social contradictions in Ukraine after the victorious war of liberation and accession to the Muscovite kingdom.", 1857));
+            books.Add(new Book("Chorna Rada1", "Panteleymon Kulish1", "a historical novel by the Ukrainian writer Panteleimon Kulish, which depicts a well-known historical event - the black council that took place in Nizhyn in 1663, recreates social contradictions in Ukraine after the victorious war of liberation and accession to the Muscovite kingdom.", 1857));
+            SelectedBook = books[0];
+
+            nextBook = new((obj) => NextBook(), (check) => books.IndexOf(SelectedBook) != books.Count - 1);
+            previusBook = new((obj) => PrevBook(), (check) => books.IndexOf(SelectedBook) != 0);
+        }
+
+
+    }
+
+
+}
